082aa54415c80384c05a6f5b9cde6c0c9ffb26cd
cp289 <cp289@wialsdkf.colby.edu>
2017-01-16 11:51:49 -0500

    made sounds directory

--- sound.py
+++ sound.py
@@ -2,7 +2,13 @@ import sys, pygame
 
 pygame.init()
 
+pygame.mixer.init()
+
+print "Loading battle music"
+
 battleMusic=pygame.mixer.Sound("sounds/battleMusic.wav")
+
+print "Playing battle music"
 
 battleMusic.play()
 
