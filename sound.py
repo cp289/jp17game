cca2deea951d3dec4925bb104ca0b989cd62f76e
cp289 <cp289@wialsdkf.colby.edu>
2017-01-16 16:01:20 -0500

    make sound.py closeable

--- sound.py
+++ sound.py
@@ -7,14 +7,15 @@ pygame.mixer.init()
 
 screen=pygame.display.set_mode((400,400))
 
-print "Loading battle music"
+#Loading battle music
 
 sound=pygame.mixer.Sound("sounds/battleMusic.ogg")
 
-print "Playing battle music"
+#Playing battle music
 
 sound.play(loops=1000)
 
 while 1:
 	for event in pygame.event.get():
-		pass
+		if event.type==pygame.QUIT:
+			sys.exit()
