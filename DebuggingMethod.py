a04a30bdb3f48141d7f946a130993db7ca591755
cp289 <cp289@wialsdkf.colby.edu>
2017-01-17 13:23:26 -0500

    create DebuggingMethod.py

---
+++ DebuggingMethod.py
@@ -0,0 +1,24 @@
+
+class DebuggingMethod:
+	
+	def __init__(self, name, time, damage):
+		self.name=name
+		
+		#proposed min of 6, max of 9
+		self.time_needed=time
+		
+		#what should the range be for the damage?
+		self.damage=damage
+
+class AskBruce(DebuggingMethod):
+	
+	def __init__(self):
+		DebuggingMethod.__init__("Ask Bruce", 5, 300)
+
+
+def main():
+	attack=DeguggingMethod()
+	
+
+
+if __name__=='__main__':main()
