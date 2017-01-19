22c6e1e66147c93172b22d0f61ab1b5997cd78b8
cp289 <cp289@wialsdkf.colby.edu>
2017-01-19 13:22:22 -0500

    small edit

--- debuggingMethod.py
+++ debuggingMethod.py
@@ -1,4 +1,8 @@
 
+# Note: stats should not be zero as this will likely cause division by zero errors.
+
+import random
+
 class DebuggingMethod:
 	
 	def __init__(self, name, time, damage):
@@ -9,18 +13,40 @@ class DebuggingMethod:
 		
 		#what should the range be for the damage?
 		self.damage=damage
+		
+	def attack(self, enemy, character): # Needs to be tested!
+		if random.random() < 0.5+(character.acc-enemy.spd)/(2*1000): # What should the highest value be for a stat?
+			enemy.takeDamage(self.damage*(character.atk/enemy.dfn))  # Likely to cause problems... adjust scale factor later.
+	
+	def attackEffects():
+		pass
 
 class AskBruce(DebuggingMethod):
-	
 	def __init__(self):
+		DebuggingMethod.__init__(self,"Ask Bruce", 4, 700)
 		
-		DebuggingMethod.__init__(self,"Ask Bruce", 5, 300)
+class AskSomeone(DebuggingMethod):
+	def __init__(self):
+		DebuggingMethod.__init__(self,"Ask Someone", 5, 500)
 		
+class PrintStatements(DebuggingMethod):
+	def __init__(self):
+		DebuggingMethod.__init__(self,"Use Print Statements", 3, 300)
+		
+class Google(DebuggingMethod):
+	def __init__(self):
+		DebuggingMethod.__init__(self,"Google Search", 1, 100)
+
+class TakeBreak(DebuggingMethod):
+	def __init__(self):
+		DebuggingMethod.__init__(self,"Take a Break", 3, 0)
+		
+	def attackEffects():
+		pass	# Restore 40% max health.
 	
 
 def main():
 	attack=AskBruce()
-	
 
 
 if __name__=='__main__':
